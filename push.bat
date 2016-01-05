0c5b1cfcc3d7d177e4ba00c4bd5954676f5d63a8
machdmach <machdmach@yahoo.com>
2016-01-05 09:11:15 -0800

    a

--- push.bat
+++ push.bat
@@ -3,4 +3,4 @@ git add .
 git commit -m "a"
 git push
 
-cmd /k "C:\program files\internet explorer\iexplore.exe" http://google.com
+REM cmd /k "C:\program files\internet explorer\iexplore.exe" http://google.com
